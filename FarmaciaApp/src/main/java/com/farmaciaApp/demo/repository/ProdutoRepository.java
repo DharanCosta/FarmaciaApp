594acc1deadb270ec53705ae9fc91cb1e27acdd7
Ingrid Kis <ingrid.kr@hotmail.com>
2022-02-24 14:24:50 -0300

    @ingrid-kis [Task: Creating Model, Repository and Controller of Produto]

---
+++ FarmaciaApp/src/main/java/com/farmaciaApp/demo/repository/ProdutoRepository.java
@@ -0,0 +1,10 @@
+package com.farmaciaApp.demo.repository;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.farmaciaApp.demo.model.ProdutoModel;
+
+public interface ProdutoRepository extends JpaRepository <ProdutoModel, Long> {
+	public List <ProdutoModel> findAllByNomeCointainingIgnoranceCase (String nome);
+
+}
